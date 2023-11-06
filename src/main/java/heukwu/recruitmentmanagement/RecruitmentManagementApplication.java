48ffdd8cdc769def5ee142e2ef57a05b904f4fd2
heukwu <yoo4068@gmail.com>
2023-11-06 22:52:42 +0900

    initial commit

---
+++ src/main/java/heukwu/recruitmentmanagement/RecruitmentManagementApplication.java
@@ -0,0 +1,13 @@
+package heukwu.recruitmentmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecruitmentManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/heukwu/recruitmentmanagement/RecruitmentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package heukwu.recruitmentmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
