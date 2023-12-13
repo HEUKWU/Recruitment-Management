129b7fd98e4d522d29b3840dac6ba8a9279b54da
heukwu <yoo4068@gmail.com>
2023-12-13 14:37:37 +0900

    Test: postService 저장 실패 테스트 코드

--- src/test/java/heukwu/recruitmentmanagement/service/PostServiceTest.java
+++ src/test/java/heukwu/recruitmentmanagement/service/PostServiceTest.java
@@ -6,6 +6,7 @@ import heukwu.recruitmentmanagement.entity.Post;
 import heukwu.recruitmentmanagement.repository.CompanyRepository;
 import heukwu.recruitmentmanagement.repository.PostRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -15,6 +16,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -53,6 +55,7 @@ public class PostServiceTest {
     }
 
     @Test
+    @DisplayName("올바른 회사id와 요청 dto로 게시글을 생성했을 때 생성하고 응답된 게시글의 skill 필드는 요청 dto의 skill 필드와 같다.")
     void createPostTest() {
 
         //given
@@ -70,5 +73,23 @@ public class PostServiceTest {
 
         //then
         assertThat(post.getSkill()).isEqualTo(dto.getSkill());
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 회사id로 게시글을 생성했을  예외가 발생한다.")
+    void createPostNoCompany() {
+
+        //given
+        when(companyRepository.findById(4L)).thenReturn(Optional.empty());
+
+        //when
+        PostDto.Req dto = PostDto.Req.builder()
+                .position("Back End")
+                .skill("Spring")
+                .description("Java Spring Back End")
+                .build();
+
+        //then
+        assertThatThrownBy(() -> postService.createPost(4L, dto)).isInstanceOf(IllegalArgumentException.class);
     }
 }
