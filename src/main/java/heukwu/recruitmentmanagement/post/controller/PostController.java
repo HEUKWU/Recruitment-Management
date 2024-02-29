4bf795a70ead4890db6ea8cd300c697909460fbe
heukwu <yoo4068@gmail.com>
2024-02-29 15:46:31 +0900

    [RM-016] Swagger 적용

--- src/main/java/heukwu/recruitmentmanagement/post/controller/PostController.java
+++ src/main/java/heukwu/recruitmentmanagement/post/controller/PostController.java
@@ -3,6 +3,8 @@ package heukwu.recruitmentmanagement.post.controller;
 import heukwu.recruitmentmanagement.post.service.Post;
 import heukwu.recruitmentmanagement.post.service.PostService;
 import heukwu.recruitmentmanagement.post.service.PostWithOtherPosts;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Tag(name = "post", description = "채용 공고")
 @RestController
 @RequiredArgsConstructor
 @Slf4j
@@ -17,6 +20,7 @@ public class PostController {
 
     private final PostService postService;
 
+    @Operation(summary = "채용 공고 검색 조회")
     @GetMapping("/post")
     public List<PostResponse.GetList> getAllPost(PostSearch search, @RequestParam int size, Long cursorId) {
         List<Post> postList = postService.getAllPost(search, size, cursorId);
@@ -26,6 +30,7 @@ public class PostController {
                 .collect(Collectors.toList());
     }
 
+    @Operation(summary = "채용 공고 상세 조회")
     @GetMapping("/post/{postId}")
     public PostResponse.Get getPost(@PathVariable Long postId) {
         PostWithOtherPosts post = postService.getPost(postId);
@@ -33,6 +38,7 @@ public class PostController {
         return PostResponse.Get.from(post);
     }
 
+    @Operation(summary = "채용 공고 생성")
     @PostMapping("/post/{companyId}")
     public PostResponse.Create createPost(
             @PathVariable Long companyId,
@@ -44,6 +50,7 @@ public class PostController {
         return PostResponse.Create.from(createdPost);
     }
 
+    @Operation(summary = "채용 공고 수정")
     @PutMapping("/post/editing/{postId}")
     public PostResponse.Edit editPost(@PathVariable Long postId, @RequestBody PostRequest.Edit request) {
         Post requestDto = request.toDomain();
@@ -52,6 +59,7 @@ public class PostController {
         return PostResponse.Edit.from(responseDto);
     }
 
+    @Operation(summary = "채용 공고 삭제")
     @DeleteMapping("/post/deleting/{postId}")
     public void deletePost(@PathVariable Long postId) {
         postService.deletePost(postId);
